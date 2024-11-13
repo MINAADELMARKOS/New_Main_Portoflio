e80d9ae026cf73ab637d8163c43331aa1ed9b19f
Mina Adel <mina.markos6565@gmail.com>
2024-11-13 02:29:12 +0200

    upload all files

---
+++ contact_process.php
@@ -0,0 +1,57 @@
+<?php
+
+    $to = "rockybd1995@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $subject = $_REQUEST['subject'];
+    $number = $_REQUEST['number'];
+    $cmessage = $_REQUEST['message'];
+
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $subject = "You have a message from your Bitmap Photography.";
+
+    $logo = 'img/logo.png';
+    $link = '#';
+
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+	$body .= "</td></tr></thead><tbody><tr>";
+	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "</tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td></td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+    $send = mail($to, $subject, $body, $headers);
+
+?>
+<!-- send the email get in touch -->
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $phone = htmlspecialchars($_POST['phone']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "karenfreelacerr@gmail.com";
+    $subject = "New Form Submission";
+    $body = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage:\n$message";
+    $headers = "From: $email\r\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Your message has been sent successfully!";
+    } else {
+        echo "Sorry, there was an error sending your message.";
+    }
+}
+?>
